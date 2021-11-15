c953c7e2ceb4e7a03ef3cb8da83dfb59aec319c9
MichalKrepelka <krepelka.michal@gmail.com>
2021-11-15 12:29:39 +0100

    PRidano stftm. Stale nefunguje nas. cov. matic.

---
+++ prednaska-05/sftfm.m
@@ -0,0 +1,19 @@
+function S = sftfm(X, hop, N, window)
+    % Provede stft pro vsechny senzory.
+    % X - signal ze senzoru
+    % hop - posun dvou okenek (odvozuji z nej overlap)
+    % N - parametr FFTLength stft
+    % window - synthesis window delky N
+    % vraci S -nastackovane vysledky stft pro jednotlive senzory 
+    
+    overlap = N - hop;  % prekryv dvou okenek
+
+    no_sensors = size(X,1);
+    no_frames = floor(1+((size(X,2)-N)/hop));
+    no_frequencies = N;
+    S = zeros(no_sensors, no_frequencies, no_frames);
+    for i=1:no_sensors
+        S(i,:,:) = stft(X(i,:), 'OverlapLength', overlap, 'FFTLength', N, 'Window', window);
+    end
+end
+
--- prednaska-05/uloha.m
+++ prednaska-05/uloha.m
@@ -7,22 +7,22 @@ load('F01_421C0201_BUS.mat')
 
 N = 128;     % delka okenka
 hop = 64;  % posunuti dalsiho okenka
-overlap = N - hop;  % prekryv dvou okenek
+window = rectwin(N); % zvolim stejne okno pro stft i istft
 
 % prevedu do casove frekvencnio oblasti pomoci stfm
-% Note: Nemelo by se pouzit spis mstfm z 1. prednasky?
-X_stfm = stft(X(1,:), 'OverlapLength', overlap, 'FFTLength', N, 'Window', rectwin(N));
-S_stfm = stft(S(1,:), 'OverlapLength', overlap, 'FFTLength', N, 'Window', rectwin(N));
+X_stfm = sftfm(X, hop, N, window);
+S_stfm = sftfm(S, hop, N, window);
 
-size(X_stfm) % frekvencni rozliseni(delka fft) x pocet framu(delka)
+% size(X_stfm) % pocet senzoru x frekvencni rozliseni(delka fft) x pocet framu(delka)
 
 % na kazde frekvenci spocitam beamformer a aplikuju ho
 aux = zeros(size(X_stfm));
-for i=1:size(X_stfm, 1)
-    C_X = cov(X_stfm(i,:));
-    C_SX = cov(S_stfm(i,:), X_stfm(1,:));
-    W_MMSE = inv(C_X) * C_SX;
-    aux(i,:) = W_MMSE' * X_stfm(i,:); % w'*x = s
+for i=1:size(X_stfm, 2)
+    C_X = cov(squeeze(X_stfm(i,:,:)));
+    C_X_inv = inv(C_X);
+    C_SX = cov(squeeze(S_stfm(i,:,:)), squeeze(X_stfm(i,:,:)));
+    W_MMSE = C_X_inv * C_SX;
+    aux(i,:,:) = W_MMSE' * squeeze(X_stfm(i,:,:)); % w'*x = s
 end
 
-X_istft = istft(aux, 'OverlapLength', overlap, 'FFTLength', N, 'Window', rectwin(N));
+% X_istft = istft(aux, 'OverlapLength', overlap, 'FFTLength', N, 'Window', rectwin(N));
