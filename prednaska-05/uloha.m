652407f2c217a0ed48c0617489af4f4a0cd77e3c
MichalKrepelka <krepelka.michal@gmail.com>
2021-11-15 16:27:23 +0100

    Pridana funkce pro istftm. Rozmerova chyba neopravena

--- prednaska-05/uloha.m
+++ prednaska-05/uloha.m
@@ -10,19 +10,21 @@ hop = 64;  % posunuti dalsiho okenka
 window = rectwin(N); % zvolim stejne okno pro stft i istft
 
 % prevedu do casove frekvencnio oblasti pomoci stfm
-X_stfm = sftfm(X, hop, N, window);
-S_stfm = sftfm(S, hop, N, window);
+X_stftm = sftfm(X, hop, N, window);
+S_stftm = sftfm(S, hop, N, window);
 
 % size(X_stfm) % pocet senzoru x frekvencni rozliseni(delka fft) x pocet framu(delka)
 
 % na kazde frekvenci spocitam beamformer a aplikuju ho
-aux = zeros(size(X_stfm));
-for i=1:size(X_stfm, 2)
-    C_X = cov(squeeze(X_stfm(i,:,:)));
+aux = zeros(size(X_stftm));
+for i=1:size(X_stftm, 2)
+    C_X = cov(squeeze(X_stftm(i,:,:)));
     C_X_inv = inv(C_X);
-    C_SX = cov(squeeze(S_stfm(i,:,:)), squeeze(X_stfm(i,:,:)));
+    C_SX = cov(squeeze(S_stftm(i,:,:)), squeeze(X_stftm(i,:,:)));
     W_MMSE = C_X_inv * C_SX;
-    aux(i,:,:) = W_MMSE' * squeeze(X_stfm(i,:,:)); % w'*x = s
+    aux(i,:,:) = W_MMSE' * squeeze(X_stftm(i,:,:)); % w'*x = s
 end
 
-% X_istft = istft(aux, 'OverlapLength', overlap, 'FFTLength', N, 'Window', rectwin(N));
+X_istftm = istftm(X_stfm, hop, N, window);
+
+% soundsc(X_istftm(1,:), Fs)
