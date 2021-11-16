a3e4d61bb10166615df373a958c842bfc4c69ca2
MichalKrepelka <krepelka.michal@gmail.com>
2021-11-16 10:40:06 +0100

    Vytazeni squeeze do promenne

--- prednaska-05/uloha.m
+++ prednaska-05/uloha.m
@@ -18,13 +18,18 @@ S_stftm = sftfm(S, hop, N, window);
 % na kazde frekvenci spocitam beamformer a aplikuju ho
 aux = zeros(size(X_stftm));
 for i=1:size(X_stftm, 2)
+    X_f = squeeze(X_stftm(:,i,:));
+    S_f = squeeze(S_stftm(:,i,:));
+
 %     C_X = cov(squeeze(X_stftm(:,i,:)));
-    C_X = squeeze(X_stftm(:,i,:)) * squeeze(X_stftm(:,i,:))' / length(squeeze(X_stftm(:,i,:)));
+    C_X = X_f * X_f' / length(X_f);
     C_X_inv = inv(C_X);
+
 %     C_SX = cov(squeeze(S_stftm(:,i,:)), squeeze(X_stftm(:,i,:)));
-    C_SX = squeeze(S_stftm(:,i,:)) * squeeze(X_stftm(:,i,:))' / length(squeeze(X_stftm(:,i,:)));
+    C_SX = S_f * X_f' / length(X_f);
+    
     W_MMSE = C_X_inv * C_SX;
-    aux(:,i,:) = W_MMSE' * squeeze(X_stftm(:,i,:)); % w'*x = s
+    aux(:,i,:) = W_MMSE' * X_f; % w'*x = s
 end
 
 S_aux = istftm(aux, hop, N, window);
