6a0df92ba71d0654c7ee12e90a14cbfe6b3ecea3
MichalKrepelka <krepelka.michal@gmail.com>
2021-11-25 17:21:39 +0100

    Opraven chybny vypocet rekonstrukce X Kvuli tomu se muselo trochu upravit cele reseni

--- prednaska-07/uloha01_PCA.m
+++ prednaska-07/uloha01_PCA.m
@@ -8,17 +8,19 @@ load("EKG3channels_sinus.mat")
 C = x*x'/length(x); 
 [V D] = eig(C); 
 
-% Vymazu komponentu s nejmensi energii (1. radek Z)
-Z = V'*x; % spoctu vsechny hl. komponenty
-Z(1,:)=0;
-XX = V' * Z; % transpozice jako inverze (viz slidy)
-eegplot(XX, 'title', 'Odstranena komponenta s nejnizsi energii')
+% Dva zpusoby, jak redukovat komponenty
 
-% Vymazu komponentu s nejmensi energii (1. sloupec V)
-VV = V(:,2:3);
-Z = VV'*x; % spoctu hl. komponenty vybrane v predchozim kroku
-XX = VV * Z; % transpozice jako inverze (viz slidy)
-eegplot(XX, 'title', 'Odstranena komponenta s nejnizsi energii')
+% Vymazu komponentu s nejmensi energii (1. radek Z)
+% Z = V'*x; % spoctu vsechny hl. komponenty
+% Z(1,:)=0;
+% XX = V * Z; % transpozice jako inverze (viz slidy)
+% eegplot(XX, 'title', 'Odstranena komponenta s nejnizsi energii')
+% 
+% % Vymazu komponentu s nejmensi energii (1. sloupec V)
+% VV = V(:,2:3);
+% Z = VV'*x; % spoctu hl. komponenty vybrane v predchozim kroku
+% XX = VV * Z; % transpozice jako inverze (viz slidy)
+% eegplot(XX, 'title', 'Odstranena komponenta s nejnizsi energii')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -26,18 +28,18 @@ eegplot(XX, 'title', 'Odstranena komponenta s nejnizsi energii')
 % Z = V'*x; % spoctu vsechny hl. komponenty
 % Z(1,:)=0;
 % Z(2,:)=0;
-% XX = V' * Z; % transpozice jako inverze (viz slidy)
+% XX = V * Z; % transpozice jako inverze (viz slidy)
 % eegplot(XX, 'title', 'Nejsilnejsi komponenta')
 
 % Dve nejslabsi komponenty - sum a divna amplituda, moc zubate
 % Z = V'*x; % spoctu vsechny hl. komponenty
 % Z(3,:)=0;
-% XX = V' * Z; % transpozice jako inverze (viz slidy)
+% XX = V * Z; % transpozice jako inverze (viz slidy)
 % eegplot(XX, 'title', 'Dve nejslabsi komponenty')
 
 % Pouze nejslabsi komponenta
 Z = V'*x; % spoctu vsechny hl. komponenty
 Z(2,:)=0;
 Z(3,:)=0;
-XX = V' * Z; % transpozice jako inverze (viz slidy)
+XX = V * Z; % transpozice jako inverze (viz slidy)
 eegplot(XX, 'title', 'Pouze nejslabsi komponenta')
--- prednaska-07/uloha02_PCA.m
+++ prednaska-07/uloha02_PCA.m
@@ -16,32 +16,45 @@ C = X*X'/length(X);
 Z = V'*X; % spoctu vsechny hl. komponenty
 
 % Na senzoru 5 je zda se videt srdce ditete
-% Z(1:2,:)=0; 
-% Z(8:end,:)=0; 
-% XX = V' * Z; % transpozice jako inverze (viz slidy)
-% eegplot(XX, 'title', 'Komponenty 3-7')
+% Z(1:2,:)=0; % 1 a 2 je sum
+% Z(8:end,:)=0; % 8 je matka 
+% X1 = V * Z; % transpozice jako inverze (viz slidy)
+% eegplot(X1, 'title', 'Komponenty 3-7')
 
 % Mezi 3-7 a 4-7 neni viditelny rozdil => 3 je sum
 % Z(1:3,:)=0;
 % Z(8:end,:)=0;
-% XX = V' * Z;
-% eegplot(XX, 'title', 'Komponenty 4-7')
+% X2 = V * Z;
+% eegplot(X2, 'title', 'Komponenty 4-7')
 
 % Mezi 4-6 a 4-7 je videt rozdil hlavne na senzorech matky => 7 je matka 
-Z(1:3,:)=0;
-Z(7:end,:)=0;
-X1 = V' * Z;
-eegplot(X1, 'title', 'Komponenty 4-6')
+% Z(1:3,:)=0;
+% Z(7:end,:)=0;
+% X3 = V * Z;
+% eegplot(X3, 'title', 'Komponenty 4-6')
 
-% 5 signal je ciste srdce ditete, ale prijde mi, ze ztraci detaily
+% 4 komponenta obsahuje nejakou informaci od ditete, muze byt sum
+% Z(1:4,:)=0;
+% Z(7:end,:)=0;
+% X4 = V * Z;
+% eegplot(X4, 'title', 'Komponenty 5-6')
+
+% 6 obsahuje matku
+Z(1:3,:)=0;
+Z(6:end,:)=0;
+X5 = V * Z;
+eegplot(X5, 'title', 'Komponenty 4-5')
+
+% 5 signal je ciste srdce ditete, 4 je asi opravdu sum
 Z(1:4,:)=0; % sum
 Z(6:end,:)=0; % matka
-X2 = V' * Z;
-eegplot(X2, 'title', 'Komponenta 5')
+X6 = V * Z;
+eegplot(X6, 'title', 'Komponenta 5')
 
 % souhrn pro prehled
-XXX = zeros(3,size(X,2));
+XXX = zeros(4,size(X,2));
 XXX(1,:) = X(2,:); % originalni signal ze 2. senzoru
-XXX(2,:) = X1(5,:); % komponenta 4-6 z 5. senzoru
-XXX(3,:) = X2(5,:); % komponenta 5 z 5. senzoru
+XXX(2,:) = X6(2,:); % komponenta 5 z 2. senzoru
+XXX(3,:) = X6(5,:); % komponenta 5 z 5. senzoru
+XXX(4,:) = X6(6,:); % komponenta 5 z 6. senzoru
 eegplot(XXX, 'title', 'Souhrn 2. orig senzor a 5. po vycisteni')
