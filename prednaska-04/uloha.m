4c92c06bd318cc1f92529cb4df41fd22871236ab
MichalKrepelka <krepelka.michal@gmail.com>
2021-11-25 16:24:17 +0100

    Pridana vzorkovaci frekvence

--- prednaska-04/uloha.m
+++ prednaska-04/uloha.m
@@ -5,12 +5,14 @@ m = 4;  % pocet senzoru
 % budu mit ULA, tj. ve 2D
 P = zeros(2,m); % pripravim si polohu senzoru (vektor z pocatku)
 for i=1:m
-    P(1, i) = i-1;
+    P(1, i) = 0.05 * (i-1); % dam od sebe senzory 5 cm
 end
 
 % Nastavim beamformer, tj. M1,...,Mm
 % M = (m:-1:1) ./ m; % zpozdeni po m-tinach
-M = (1:m);
+% M = (1:m);
+% M = randn(1,m);
+M = zeros(1,m); % nej prjem z primeho smeru
 
 % Zvolim parametry
 fs = 16e3;  % vzorkovaci frekvence (Hz)
@@ -18,8 +20,8 @@ c = 343;    % Rychlost sireni signalu (= cca rychlost zvuku) (m/s)
 
 % Zakresli magnitudovou charakteristiku prenosove funkce v zavislosti na
 % uhlu sireni signalu pro zvolenou frekvenci
-theta = 1000; % zvolena frekvence 1kHz
-theta = 2 * pi * theta ;%/ fs; % normalizace % nahodnym zakomentovanim fs se zda, ze dela to, co chci
+theta = 3000; % zvolena frekvence 1kHz
+theta = 2 * pi * theta / fs; % normalizace - vydelenim fs ziskam bezrozmerovou velicinu 
 
 rad = pi / 180; % Neni lepsi 2pi vydelit Fs?
 alpha_ticks = 0:rad:2*pi-rad; % vsechny uhly pro zavislost, odcitam, abych mel 360
@@ -27,7 +29,7 @@ alpha_ticks = 0:rad:2*pi-rad; % vsechny uhly pro zavislost, odcitam, abych mel 3
 % vektor ma jednotkovou delku a znam uhel alfa
 u = [1 * cos(alpha_ticks); 1 * sin(alpha_ticks)];
 
-D = u' * P / c; % posunuti
+D = (u' * P / c) * fs; % posunuti - vynasobenim fs ziskam bezrozmerovou velicinu
 
 % Prenosova funkce
 H = zeros(size(D, 1), 1);
