00ba0428343e22e7c11df741d5f42fdc3cdc6100
MichalKrepelka <krepelka.michal@gmail.com>
2021-11-30 21:28:07 +0100

    Nastrel reseni 2. bodu

--- prednaska-01/uloha02.m
+++ prednaska-01/uloha02.m
@@ -3,13 +3,17 @@ clear; clc; close all;
 % nacteni souboru
 load('F01_421C0201_BUS.mat')
 
-m=3;    % pocet filtru
+m=size(X,1);    % pocet filtru
 L=16;   % delka filtru
 
 %%% bod 1 ulohy %%%
 
+% pripravim si m nahodnych filtru delky L
+h = randn(m, L);
+
 % dam si za sebe m nahodnych filtru do vektoru
-h_tilde = randn(m*L,1);
+h_tilde = h';
+h_tilde = h_tilde(:); % oprator : spoji sloupce matice do sloupcoveho vektoru
 
 % pripravim si matici vstupu
 Xt_tilde = [];  % bude mit rozmery m*L x length(X)
@@ -19,15 +23,44 @@ for i=1:m
 end
 
 % vystup pro bod 1 teto ulohy
-y1 = h_tilde' * Xt_tilde;
+Y1 = h_tilde' * Xt_tilde;
 
 %%% bod 2 ulohy %%%
 
 % spoctu transformace jednotlivych filtru
-hitheta = mydft(h_tilde);   %tohle se lisi
-tmp=mydft(h_tilde(1:L));
+% vlozim si stftm a istftm z 5. prednasky
+addpath(genpath("../prednaska-05"));
+
+% spoctu transformace jednotlivych filtru h_i
+h_theta = zeros(size(h));
+for i=1:size(h,1)
+    h_theta(i,:) = fft(h(i,:));
+end
+
+% prevedu X do casove frekvencnio oblasti pomoci stfm
+N = L;     % delka okenka odpovida delce filtru
+hop = 4;  % posunuti dalsiho okenka
+window = rectwin(N); % zvolim stejne okno pro stft i istft
+
+X_theta = sftfm(X, hop, N, window); % 6x16x56666
+
+% pro kazde theta spoctu radek Y
+M = size(X_theta,3); % pocet framu
+
+Y_theta = zeros(m, M); % size(X_theta,1), size(X_theta,3)
+for i=1:L % size(X_theta,2)
+    x = squeeze(X_theta(:,i,:));
+    y = h_theta(:, i)' * x;
+    Y_theta(i,:) = y;
+end
+Y2 = reshape(Y_theta, 1, L, M); % 1 x size(Y_theta)
+% prevedu zpet
+Y2 = real(istftm(Y2, hop, N, window));
 
 %%% bod 3 ulohy %%%
 % Vystupy nebudou stejne, ptz bod 1 byl udelan linearni konvoluci
 % a bod 2 kruhovou, navic pouzito framovani
-
+plot(Y1)
+hold on
+plot(Y2)
+legend('Y1','Y2')
